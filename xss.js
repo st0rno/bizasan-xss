601983670b7dca8e9e0844d157a243188afde182
st0rno <56682392+st0rno@users.noreply.github.com>
2021-12-01 20:51:09 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,2 +1,2 @@
-console.log('xss-v3');
-window.open('https://www.jan-kolarik.cz');
+console.log('xss-v4');
+window.location.href = 'https://edu.anarcho-copy.org/Against%20Security%20&%20%20Self%20Security/XSS%20CHEAT%20SHEET%202020%20edition.pdf';
