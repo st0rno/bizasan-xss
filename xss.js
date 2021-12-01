bdafaf129267643647b381856ad1b6a71f75fe3a
st0rno <56682392+st0rno@users.noreply.github.com>
2021-12-01 21:06:50 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,3 +1,5 @@
 console.log('xss');
 window.open('https://edu.anarcho-copy.org/Against%20Security%20&%20%20Self%20Security/XSS%20CHEAT%20SHEET%202020%20edition.pdf');
 $('#searchDataInput').val('gay porn'); 
+$('.card-title').text('Lorem Ipsum');
+$('.card-text').text('Dolor Sit Amet');
