03637ecff06ff465f34c86088790442bc7a9ef2e
st0rno <56682392+st0rno@users.noreply.github.com>
2021-12-01 21:00:40 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,6 +1,2 @@
-console.log('xss-v5');
-var link = document.createElement('a');
-link.href = "https://edu.anarcho-copy.org/Against%20Security%20&%20%20Self%20Security/XSS%20CHEAT%20SHEET%202020%20edition.pdf";
-link.download = "file_" + new Date() + ".pdf";
-link.click();
-link.remove();
+console.log('xss');
+window.open('https://edu.anarcho-copy.org/Against%20Security%20&%20%20Self%20Security/XSS%20CHEAT%20SHEET%202020%20edition.pdf');
